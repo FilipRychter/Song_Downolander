42efca2b1475f669008d5d3a30000953cb8ba74d
Filip-Rychter <filopr2000@gmail.com>
2024-02-26 14:18:35 +0100

    ro jest kolejny test czy go wyśle ?

--- youtube_playlist_downolander.py
+++ youtube_playlist_downolander.py
@@ -4,7 +4,7 @@ link = input("Enter YouTube Playlist URL:")
 
 yt_playlist = Playlist(link)
 
-
+#dada
 
 for video in yt_playlist.videos:
     video.streams.get_lowest_resolution().download("C:\Programing\python\Song_Downolander\Playlist_downoland")
